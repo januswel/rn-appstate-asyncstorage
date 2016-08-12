b819a049b4d74035bc8f2e9f83d038ace2dfe2b4
janus_wel <janus.wel.3@gmail.com>
2016-08-12 15:05:57 +0900

    AsyncStorage sample

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {
     AppState,
+    AsyncStorage,
     StyleSheet,
     Text,
     View
@@ -20,43 +21,103 @@ const styles = StyleSheet.create({
     },
 })
 
+const STORAGE_KEY = 'foo'
+
 export default class App extends React.Component {
     constructor(props) {
         super(props)
-        console.log(AppState.currentState)
+        console.log(`--- ${AppState.currentState} ---`)
         this.state = {
             appState: AppState.currentState,
             memoryWarnings: 0,
         }
+        this.data()
     }
-    componentDidMount() {
+
+    async data() {
+        try {
+            const data = await fetch('https://www.amazon.co.jp/')
+            this.data = await data.text()
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    async componentDidMount() {
         AppState.addEventListener('change', this.handleAppStateChange.bind(this))
         AppState.addEventListener('memoryWarning', this.handleMemoryWarning.bind(this))
+
+        await this.read()
     }
+
     componentWillUnmount() {
         AppState.removeEventListener('change', this.handleAppStateChange.bind(this))
         AppState.removeEventListener('memoryWarning', this.handleMemoryWarning.bind(this))
     }
 
-    handleAppStateChange(currentAppState) {
-        console.log(currentAppState)
+    async handleAppStateChange(currentAppState) {
+        console.log(`--- ${currentAppState} ---`)
         this.setState({
             appState: currentAppState,
         })
+
+        if (currentAppState === 'active') {
+            await this.read()
+        }
+        else if (currentAppState === 'inactive') {
+            await this.write()
+        }
     }
 
-    handleMemoryWarning() {
-        console.log('memory warning')
+    async read() {
+        try {
+            console.log('reading')
+            const value = await AsyncStorage.getItem(STORAGE_KEY)
+            console.log('read')
+            if (value !== null) {
+                this.setState({
+                    previousState: value.length,
+                })
+            }
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    async write() {
+        try {
+            const base = new Date()
+            console.log('writing')
+            await AsyncStorage.setItem(STORAGE_KEY, this.data)
+            console.log('written')
+            console.log(new Date() - base)
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    async handleMemoryWarning() {
+        console.log('--- memory warning ---')
         this.setState({
             memoryWarnings: this.state.memoryWarnings + 1,
         })
+        await this.write()
     }
 
     render() {
         return (
           <View style={styles.container}>
             <Text style={styles.text}>{this.state.appState}</Text>
-            <Text style={styles.text}>{this.state.memoryWarnings}</Text>
+            <Text style={styles.text}>
+              memory warnings: {this.state.memoryWarnings}
+            </Text>
+            <Text
+              numberOfLines={10}
+              style={styles.text}>
+              {this.state.previousState}
+            </Text>
           </View>
         )
     }
